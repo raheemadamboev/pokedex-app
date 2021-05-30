24c4c6e9bcc67442460ca45abcbb39bde5ea8e1d
Raheem <67774375+raheemadamboev@users.noreply.github.com>
2021-05-30 18:45:41 +0500

    theme

--- app/src/main/java/xyz/teamgravity/pokedex/ui/theme/Theme.kt
+++ app/src/main/java/xyz/teamgravity/pokedex/ui/theme/Theme.kt
@@ -5,38 +5,28 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = Color.Yellow,
+    background = Color(0xFF101010),
+    onBackground = Color.White,
+    surface = Color(0xFF303030),
+    onSurface = Color.White
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
+    primary = Color.Blue,
+    background = LightBlue,
     onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+    surface = Color.White,
+    onSurface = Color.Black
 )
 
 @Composable
-fun PokedexTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
-
+fun PokedexTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
     MaterialTheme(
-        colors = colors,
+        colors = if (darkTheme) DarkColorPalette else LightColorPalette,
         typography = Typography,
         shapes = Shapes,
         content = content
