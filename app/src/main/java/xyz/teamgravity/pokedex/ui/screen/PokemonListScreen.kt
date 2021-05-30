25207676b50d4ad65a359bf104f6619fc5f5b1a8
Raheem <67774375+raheemadamboev@users.noreply.github.com>
2021-05-30 20:36:01 +0500

    pokedex list

--- app/src/main/java/xyz/teamgravity/pokedex/ui/screen/PokemonListScreen.kt
+++ app/src/main/java/xyz/teamgravity/pokedex/ui/screen/PokemonListScreen.kt
@@ -2,25 +2,41 @@ package xyz.teamgravity.pokedex.ui.screen
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.focus.FocusState
 import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import coil.request.ImageRequest
+import com.google.accompanist.coil.CoilImage
 import xyz.teamgravity.pokedex.R
+import xyz.teamgravity.pokedex.arch.viewmodel.PokemonListViewModel
+import xyz.teamgravity.pokedex.model.PokedexModel
+import xyz.teamgravity.pokedex.ui.theme.RobotoCondensed
 
 @Composable
 fun PokemonListScreen(
@@ -84,5 +100,84 @@ fun SearchBar(
                 modifier = Modifier.padding(horizontal = 20.dp, vertical = 12.dp)
             )
         }
+    }
+}
+
+@Composable
+fun PokedexCard(
+    entry: PokedexModel,
+    navController: NavController,
+    modifier: Modifier = Modifier,
+    viewmodel: PokemonListViewModel = hiltViewModel()
+) {
+    val defaultDominantColor = MaterialTheme.colors.surface
+
+    var dominantColor by remember { mutableStateOf(defaultDominantColor) }
+
+    Box(
+        modifier = modifier
+            .shadow(5.dp, RoundedCornerShape(10.dp))
+            .clip(RoundedCornerShape(10.dp))
+            .aspectRatio(1f)
+            .background(Brush.verticalGradient(listOf(dominantColor, defaultDominantColor)))
+            .clickable {
+                navController.navigate("pokemon_detail_screen/${dominantColor.toArgb()}/${entry.name}")
+            }
+    ) {
+        CoilImage(
+            request = ImageRequest.Builder(LocalContext.current)
+                .data(entry.imageUrl)
+                .target {
+                    viewmodel.calculateDominateColor(it) { color ->
+                        dominantColor = color
+                    }
+                }.build(),
+            contentDescription = "raheem",
+            fadeIn = true,
+            modifier = Modifier
+                .size(120.dp)
+                .align(Alignment.Center)
+        ) {
+            CircularProgressIndicator(
+                color = MaterialTheme.colors.primary,
+                modifier = Modifier.scale(0.5f)
+            )
+        }
+
+        Text(
+            text = entry.name,
+            fontFamily = RobotoCondensed,
+            fontSize = 20.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
+}
+
+@Composable
+fun PokedexRow(
+    rowIndex: Int,
+    entries: List<PokedexModel>,
+    navController: NavController
+) {
+    Column {
+        Row {
+            PokedexCard(
+                entry = entries[rowIndex * 2],
+                navController = navController,
+                modifier = Modifier.weight(1f)
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            if (entries.size >= rowIndex * 2 + 2) {
+                PokedexCard(
+                    entry = entries[rowIndex * 2 + 1],
+                    navController = navController,
+                    modifier = Modifier.weight(1f)
+                )
+            } else {
+                Spacer(modifier = Modifier.weight(1f))
+            }
+        }
+        Spacer(modifier = Modifier.height(16.dp))
     }
 }
